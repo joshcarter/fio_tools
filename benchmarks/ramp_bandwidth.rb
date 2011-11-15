c871590d53b75e0cc47e202c453edc1f6b34a9e1
Josh Carter <public@joshcarter.com>
2011-11-15 14:12:22 -0700

    Improved bandwidth ramp test.

--- benchmarks/ramp_bandwidth.rb
+++ benchmarks/ramp_bandwidth.rb
@@ -1,18 +1,35 @@
 #!/usr/bin/env ruby
 
-rate = 100 * 1024
-step = 100 * 1024
-block_sizes = ['4k', '16k', '64k']
+block_sizes = [4 * 1024, 16 * 1024, 64 * 1024, 256 * 1024, 1024 * 1024]
 
-loop do
-  ENV['RATE'] = rate.to_s
-  ENV['BS'] = block_sizes.first
-  ENV['RATEMIN'] = (rate * 0.8).to_i.to_s
+block_sizes.each do |bs|
+  rate = 1024 * 1024
+  step = 1024 * 1024
+  rate = bs if (rate < bs)
 
-  unless system("fio ramp_bandwidth.fio")
-    puts "error running fio"
-    exit -1
+  loop do
+    ENV['RATE'] = rate.to_s
+    ENV['BS'] = bs.to_s
+    ENV['RATEMIN'] = (rate * 0.8).to_i.to_s
+
+    unless system("fio ramp_bandwidth.fio")
+      puts "fio returned error"
+      break
+    end
+
+    ["bw", "clat"].each do |log|
+      File.open("ramp_#{log}.log") do |infile|
+        File.open("ramp_merged_#{log}.log", "a") do |outfile|
+          infile.each_line do |line|
+            outfile.puts "#{line.chomp}, #{rate / 1024}"
+          end
+        end
+      end
+
+      # Reset log file
+      File.open("ramp_#{log}.log", "w").close
+    end
+
+    rate += step
   end
-
-  rate += step
 end
